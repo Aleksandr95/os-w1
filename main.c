a38eeb528d5fbc4860781428a2372f9e62278a99
Evgeny Sinelnikov <sin@altlinux.ru>
2011-02-16 04:52:03 +0300

    Add errors processing with fork()
    With new version of glibc this code has a problem.
    Notes from original fork man page:
     Since glibc version 2.3.4, the glibc wrapper function for getpid()  caches
     PIDs, so as to avoid additional system calls when a process calls getpid()
     repeatedly.  Normally this caching is invisible, but its correct operation
     relies  on  support  in  the  wrapper functions for fork(2), vfork(2), and
     clone(2): if an application bypasses the glibc wrappers for  these  system
     calls  by  using  syscall(2),  then  a  call to getpid() in the child will
     return the wrong value (to be precise: it will return the PID of the  par
     ent  process).   See also clone(2) for discussion of a case where getpid()
     may return the wrong value even when invoking clone(2) via the glibc wrap
     per function.

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 int main (int argc, char *argv[])
 {
     pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        fprintf(stderr, "fork: %s", strerror(errno));
+        return 1;
+    }
+
+    if (pid == 0)
+    {
+        pid = getpid();
+        printf("Child process with pid=%d\n", pid);
+        return 0;
+    }
+
     printf("Main process with pid=%d\n", pid);
 }
 
