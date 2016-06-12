2654f49922b4f0b3cb9f110f67c8e55c3332d6eb
Aleksandr Sergeev <sgusergeev@mail.ru>
2016-06-12 15:40:35 +0300

    second commit

--- main.c
+++ main.c
@@ -15,11 +15,11 @@ int main (int argc, char *argv[])
 
     if (pid == 0)
     {
-        pid = getpid();
         printf("Child process with pid=%d\n", pid);
+        execlp("mkdir","mkdir","test",NULL);
         return 0;
     }
-
+    pid = getpid();
     printf("Main process with pid=%d\n", pid);
 }
 
