bbedad60d88a9cfdf7362cc857b4711b730a2ea6
Aleksandr Sergeev <sgusergeev@mail.ru>
2016-06-12 16:47:08 +0300

    first commit

--- main.c
+++ main.c
@@ -5,20 +5,47 @@
 
 int main (int argc, char *argv[])
 {
+    int fd[2];
+    size_t size;
+    char resstring[14];
+    
+    if(pipe(fd) < 0)
+    {
+      printf("Can\'t create pipe\n");
+      return 1;
+      }
     pid_t pid = fork();
 
     if (pid < 0)
     {
         fprintf(stderr, "fork: %s", strerror(errno));
+         printf("Can not create child");
         return 1;
     }
 
     if (pid == 0)
     {
-        pid = getpid();
-        printf("Child process with pid=%d\n", pid);
+        close(fd[1]);
+        size = read(fd[0], resstring, 14);
+        if (size < 0)
+        {
+           printf("Can not read\n");
+           return -1;
+        }
+        printf("Yes.Can read. str = %s\n", resstring);
+        close(fd[0]);
         return 0;
+        }   
+        pid = getpid();
+        close(fd[0]);
+        size = write (fd[1], "hello , world", 14);
+        if (size != 14)
+        {
+        printf("Can not write\n");
+        return -1;
     }
-    printf("Main process with pid=%d\n",getpid());
+    close(fd[1]);
+    printf("Yes.Can write\n");
+    return 0;
 }
 
